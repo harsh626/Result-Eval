c3924f9b7ab6b1f1bffc9dcd86c215379940eb0a
Jay Rajesh Munjapara <59328819+jay-munjapara@users.noreply.github.com>
2021-05-18 18:35:33 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -5,25 +5,24 @@ import pandas as pd
 import subprocess
 import webbrowser
 import matplotlib.pyplot as plt
-#from individualPage import individualAnalysis
-#from combinedPage import combinedAnalysis
-#from fileUpload import uploadFile
-#from dataCleaning import cleaning
-from pages import cleaning,uploadFile,combinedAnalysis,individualAnalysis,passfail
+from pages import cleaning, uploadFile, combinedAnalysis, individualAnalysis, passfail
 
 st.image('Somaiya Header.png',width=500)
 st.title('Result analysis')
-st.subheader('KJ somaiya institute of engineering and IT')
-st.sidebar.title('Welcome to the result analyser App')
-
+st.subheader('K. J. Somaiya Institute Of Engineering And Information Technology')
+st.sidebar.title('Welcome to the Result Evaluation Web app')
 
 filename = uploadFile()
 st.write(filename)
+
 pd_df = cleaning(filename)
 pa_df = passfail(filename)
+
 Analyse_type=st.sidebar.radio('Analyser',('Combined Result','Individual Analysis'))
+
 if Analyse_type=='Combined Result':
     combinedAnalysis(pd_df,pa_df)
+
 if Analyse_type=='Individual Analysis':
     if filename==None:
         print("No file selected")
