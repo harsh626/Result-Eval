6c0d33c70aa0792f98438047063e930e5ea0d292
harsh626 <62324566+harsh626@users.noreply.github.com>
2021-07-11 01:59:55 -0700

    Some changes for updated individual function

--- pages.py
+++ pages.py
@@ -6,20 +6,19 @@ import plotly.graph_objects as go
 from data import create_database,fire_query
 
 # file Uploading Funtion
-
 #def uploadFile():
-#    filename = None
-#    filename = st.text_input('Enter a file path:','Cleaned Marksheet.xlsx')
-#    return filename
+#    uploaded_file = None
+#    uploaded_file = st.text_input('Enter a file path:','xyz.xlsx')
+#    return uploaded_file
 
 def uploadFile():
-	uploaded_file = st.file_uploader("Choose a file")
-	if uploaded_file is not None:
-		return uploaded_file.name
+    uploaded_file = st.file_uploader("Choose a file")
+    if uploaded_file is not None:
+        return uploaded_file.name
 
 # Cleaning Funtion
-def cleaning(filename):
-    df = pd.read_excel(filename)
+def cleaning(uploaded_file):
+    df = pd.read_excel(uploaded_file)
     # Applied Mathematics Theory
     AM_1_list = []
     for i in range(0,130):
@@ -32,13 +31,11 @@ def cleaning(filename):
         EP_1_list.append(df._get_value(i,'EP - 1'))
     EP_1_list
 
-
     #Engineering Chemistry Theory
     EC_1_list = []
     for i in range(0,130):
         EC_1_list.append(df._get_value(i,'EC - 1'))
     EC_1_list
-
 
     #Engineering Mechanics
     EM_1_list = []
@@ -127,8 +124,8 @@ def cleaning(filename):
     return pd_df
 
 
-def passfail(filename):
-    df = pd.read_excel(filename)
+def passfail(uploaded_file):
+    df = pd.read_excel(uploaded_file)
     PassFail_list = []
     for i in range(130):
         PassFail_list.append(int(df._get_value(i,'PASS_FAIL')))
@@ -151,6 +148,25 @@ def combinedAnalysis(pd_df,pa_df):
                     marker=dict(colors=colors, line=dict(color='#000000', width=2)))
     st.plotly_chart(fig)
 
+    number = int(st.text_input("Enter the number of top records to be displayed: "))
+    option = str(st.selectbox(
+    'Enter Subject to be selected',
+    ('Applied-Mathematics I' , 'Engineering Physics I' , 'Engineering Chemistry I' , 
+     'Engineering Mechanics' ,'Basic Electrical Engineering' , 'Workshop','G.P.A',
+     'Applied Mathematics Term Work','Engineering Physics Term Work',
+     'Engineering Mechanics Term Work','BEE Term Work','Engineering Chemistry Term Work')))
+
+    pd_df.sort_values(by=option, inplace=True, ascending = False)
+    sorted_list =  pd_df[option][0:number]
+    names_list = pd_df['Names'][0:number]
+    sorted_df = pd.DataFrame(
+        list(zip(
+            names_list,
+            sorted_list
+        )),
+        columns = ['Names',option])
+    st.write(sorted_df)
+
     #Bar Graphs
     agg_df = calculateAgg(pd_df)
     for count,column in enumerate(pd_df):
@@ -160,7 +176,6 @@ def combinedAnalysis(pd_df,pa_df):
                             marker_line_width=1.5, opacity=0.6)
             fig.update_layout(title_text=pd_df.columns[count+1])
             st.plotly_chart(fig)
-
 
 def calculateAgg(pd_df):
     avg_list=[]
@@ -186,16 +201,14 @@ def calculateAgg(pd_df):
     
 # Individual Analysis
 
-def individualAnalysis(pd_df,filename):
+def individualAnalysis(pd_df,uploaded_file):
     st.title("Individual Analysis")
-    
-    Name = st.text_input('Enter a Name','name')
+    Name = st.text_input('Enter a Name')
     query = 'select*from `Cleaned Marksheet` where Names='+Name
-
-    create_database(filename,pd_df)
-    individual_df = fire_query(query,filename)
+    create_database(uploaded_file,pd_df)
+    individual_df = fire_query(query,uploaded_file)
     
-    st.write(individual_df.T)
+    st.write(individual_df)
     
     
 
@@ -207,14 +220,97 @@ def individualAnalysis(pd_df,filename):
         term_work_df = pd.DataFrame(dict(
             marks=individual_df.values[0][8:],
             Subjects=individual_df.columns[8:]))
-        theory_fig = px.line_polar(theory_df, r='marks', theta='Subjects', line_close=True)
-        theory_fig.update_traces(fill='toself')
+
+        AMP = []
+        EPP = []
+        ECP = []
+        EMP = []
+        BEEP = []
+        WP = []
+        AMTWP = []
+        EPTWP = []
+        ECTWP = []
+        EMTWP = []
+        BEETWP = [] 
+        for column in individual_df.columns:
+            if column=='Applied-Mathematics I':
+                for value in list(individual_df[column]):
+                    AMP.append((value/100)*100)
+            elif column=='Engineering Physics I':
+                for value in list(individual_df[column]):
+                    EPP.append((value/75)*100)
+            elif column=='Engineering Chemistry I':
+                for value in list(individual_df[column]):
+                    ECP.append((value/75)*100)
+            elif column=='Engineering Mechanics':
+                for value in list(individual_df[column]):
+                    EMP.append((value/100)*100)
+            elif column=='Basic Electrical Engineering':
+                for value in list(individual_df[column]):
+                    BEEP.append((value/100)*100)
+            elif column=='Workshop':
+                for value in list(individual_df[column]):
+                    WP.append((value/50)*100)
+            elif column=='Applied Mathematics Term Work':
+                for value in list(individual_df[column]):
+                    AMTWP.append((value/25)*100)
+
+            elif column=='Engineering Physics Term Work':
+                for value in list(individual_df[column]):
+                    EPTWP.append((value/25)*100)
+        
+            elif column=='Engineering Mechanics Term Work':
+                for value in list(individual_df[column]):
+                    EMTWP.append((value/50)*100)
+
+            elif column=='BEE Term Work':
+                for value in list(individual_df[column]):
+                    BEETWP.append((value/50)*100)
+            elif column=='Engineering Chemistry Term Work':
+                for value in list(individual_df[column]):
+                    ECTWP.append((value/25)*100)
+
+        theory_percent_df_temp = pd.DataFrame(list(zip(
+        AMP,
+        EPP,
+        ECP,
+        EMP,
+        BEEP,
+        WP)),
+        columns =['Applied-Mathematics I' , 'Engineering Physics I' , 
+        'Engineering Chemistry I' , 'Engineering Mechanics' , 'Basic Electrical Engineering' ,
+         'Workshop'])
+
+        theory_percent_df = pd.DataFrame(dict(
+            marks=theory_percent_df_temp.values[0][0:],
+            Subjects=theory_percent_df_temp.columns[0:]))
+        
+
+        practical_percent_df_temp  = pd.DataFrame(list(zip(
+        AMTWP,
+        EPTWP,
+        EMTWP,
+        BEETWP,
+        ECTWP)),
+        columns =['Applied Mathematics Term Work','Engineering Physics Term Work',
+         'Engineering Mechanics Term Work','BEE Term Work','Engineering Chemistry Term Work'])
+
+        practical_percent_df = pd.DataFrame(dict(
+            marks=practical_percent_df_temp.values[0][0:],
+            Subjects=practical_percent_df_temp.columns[0:]))
+
+        theory_percent_fig = px.line_polar(theory_percent_df, r='marks', theta='Subjects', line_close=True,range_r=[0,100.00])
+        theory_percent_fig.update_traces(fill='toself')
+
         st.write("Theory Marks Distribution")
-        st.plotly_chart(theory_fig)
-        term_work_fig = px.line_polar(term_work_df, r='marks', theta='Subjects', line_close=True)
-        term_work_fig.update_traces(fill='toself')
+
+        st.plotly_chart(theory_percent_fig)
+
+        practical_percent_fig = px.line_polar(practical_percent_df, r='marks', theta='Subjects', line_close=True,range_r=[0,100.00])
+        practical_percent_fig.update_traces(fill='toself')
+
         st.write("Term Work Marks Distribution")
-        st.plotly_chart(term_work_fig)
+        st.plotly_chart(practical_percent_fig)
 
         temp = individual_df.values.T[1:]
         temp_df = pd.DataFrame(list(temp),columns=['Student Marks'])
@@ -227,6 +323,7 @@ def individualAnalysis(pd_df,filename):
                                 marker_line_width=1.5, opacity=0.6)
                 fig.update_layout(title_text=pd_df.columns[count+1])
                 st.plotly_chart(fig)
+
 
 
 
